85573ac601f6300857175073f8ef968489da0ff7
sunya9 <mugen.xyz@gmail.com>
2020-07-01 06:04:27 +0900

    refactor: remove nuxt-payload-extractor

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,7 +74,6 @@ const config: NuxtConfig = {
     // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
     // '@nuxtjs/bulma',
     '@nuxtjs/pwa',
-    // 'nuxt-payload-extractor',
     '@nuxtjs/axios',
     '@nuxtjs/sentry',
     '@nuxtjs/sitemap',
