4271fa705aa6fe15369251ad4661208716759147
sunya9 <mugen.xyz@gmail.com>
2020-09-19 23:47:26 +0900

    fix: mock data

--- __tests__/fixtures/tag.ts
+++ __tests__/fixtures/tag.ts
@@ -1,3 +1,9 @@
 import { Tag } from '@tryghost/content-api'
 
-export const tag: Tag = { name: 'tag', url: '/tags/tag', id: '1', slug: 'tag' }
+export const tag: Tag = {
+  name: 'tag',
+  url: '/tags/tag',
+  id: '1',
+  slug: 'tag',
+  description: ''
+}
